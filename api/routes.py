d4c91e48a2b3b66a8ebbe3e6844977da4eda0d38
Andrea Scala <andreascalaquattro+github@gmail.com>
2026-06-22 12:21:20 +0200

    feat: integrate Groq LLM in /ask endpoint

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,18 @@
 from api.schemas import SBRequest, SBResponse
 from fastapi import APIRouter
+from groq import Groq
+from core.config import GROQ_API_KEY
 
 router = APIRouter()
+client = Groq(api_key=GROQ_API_KEY)
 
 @router.post("/ask")
 def ask(request: SBRequest):
-    return SBResponse(response=request.prompt)
+    chat_completion = client.chat.completions.create(messages=[
+        {
+            "role": "user",
+            "content": request.prompt,
+        }
+    ], model="llama-3.3-70b-versatile")
+    response = chat_completion.choices[0].message.content
+    return SBResponse(response=response)
