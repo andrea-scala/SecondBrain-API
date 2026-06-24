5c94f5180e2611e5ab26ecf5eb08d7d78e74bd48
Andrea Scala <andreascalaquattro+github@gmail.com>
2026-06-24 11:58:27 +0200

    feat: add retrieval and connect RAG pipeline to /ask endpoint

--- api/routes.py
+++ api/routes.py
@@ -2,16 +2,18 @@ from api.schemas import SBRequest, SBResponse, IngestResponse
 from fastapi import APIRouter, UploadFile, File
 from groq import Groq
 from core.config import GROQ_API_KEY
-from rag.pipeline import chunking, embedding, save_to_chromadb
+from rag.pipeline import chunking, embedding, save_to_chromadb, retrieve
 router = APIRouter()
 client = Groq(api_key=GROQ_API_KEY)
 
 @router.post("/ask")
 def ask(request: SBRequest):
+    context = retrieve(request.prompt)
+    content = f"Given the context: ```{context}```, answer to the question: ```{request.prompt}```"
     chat_completion = client.chat.completions.create(messages=[
         {
             "role": "user",
-            "content": request.prompt,
+            "content": content,
         }
     ], model="llama-3.3-70b-versatile")
     response = chat_completion.choices[0].message.content
